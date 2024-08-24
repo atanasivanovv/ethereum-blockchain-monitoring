5084655f52cb1c018f68dac67fa85d8d69796ba2
atanasivanovv <atanas.ivanov@dreamix.eu>
2024-08-24 17:59:13 +0300

    refactor: use web sockets for connection with infura

--- src/config/web3.js
+++ src/config/web3.js
@@ -1,7 +1,7 @@
 import Web3 from "web3";
 import config from "./index.js";
 
-const infuraUrl = `https://mainnet.infura.io/v3/${config.infuraApiKey}`;
-const web3 = new Web3(new Web3.providers.HttpProvider(infuraUrl));
+const infuraUrl = `wss://mainnet.infura.io/ws/v3/${config.infuraApiKey}`;
+const web3 = new Web3(new Web3.providers.WebsocketProvider(infuraUrl));
 
 export default web3;
